9c6039319f690dab09c7f6a7675600f772651d6f
kirill <totoshkus@gmail.com>
2024-10-13 13:49:39 +0200

    first commit

--- script.js
+++ script.js
@@ -1,19 +1,54 @@
 function copyToClipboard() {
-    const commandText = document.querySelector('.install-command').textContent;
-    navigator.clipboard.writeText(commandText).then(() => {
-        alert('Команда скопирована в буфер обмена!');
-    }, () => {
-        alert('Ошибка при копировании команды.');
+    const textToCopy = document.querySelector('.language-bash').innerText;
+    navigator.clipboard.writeText(textToCopy).then(() => {
+        alert('Команда скопирована!');
     });
 }
 
-document.querySelectorAll('.button-link').forEach(button => {
-    button.addEventListener('mouseenter', () => {
-        button.style.transform = 'scale(1.1)';
-        button.style.boxShadow = '0 10px 20px rgba(0, 0, 0, 0.5)';
+function copyToClipboardPython() {
+    const textToCopy = document.querySelectorAll('.language-python')[0].innerText;
+    navigator.clipboard.writeText(textToCopy).then(() => {
+        alert('Python код скопирован!');
     });
-    button.addEventListener('mouseleave', () => {
-        button.style.transform = 'scale(1)';
-        button.style.boxShadow = '0 5px 15px rgba(0, 0, 0, 0.2)';
+}
+
+function copyToClipboardJava() {
+    const textToCopy = document.querySelectorAll('.language-java')[0].innerText;
+    navigator.clipboard.writeText(textToCopy).then(() => {
+        alert('Java код скопирован!');
     });
-});
+}
+
+function sendTestRequest() {
+    const model = document.getElementById('model-selector').value;
+    const message = document.getElementById('message-input').value;
+
+    if (!message) {
+        alert('Пожалуйста, введите текст для проверки.');
+        return;
+    }
+
+    const apiUrl = 'https://neurospacex-ruspamlib-serverandsite.hf.space/api/check_spam';
+    const payload = {
+        message: message,
+        model_name: model
+    };
+
+    fetch(apiUrl, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(payload)
+    })
+    .then(response => response.json())
+    .then(data => {
+        const pre = document.getElementById('api-response');
+        pre.innerText = JSON.stringify(data, null, 2);
+        Prism.highlightAll();  // подсветка ответа JSON
+    })
+    .catch(error => {
+        alert('Произошла ошибка при отправке запроса.');
+        console.error('Ошибка:', error);
+    });
+}
